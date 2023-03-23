2af23664e4d019cc99295f90a5d41cea89cbca60
Youngwoo Choi <yw823x@daum.net>
2023-03-23 15:30:04 +0900

    init

---
+++ src/main/java/com/ll/sbr/SbrApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbrApplication.class, args);
+    }
+
+}
