67ff6e4c241483d8610cc7affdae533daf8d8b8f
uoise <yw823x@daum.net>
2023-03-27 14:24:48 +0900

    question_list added

---
+++ src/main/java/com/ll/sbr/question/controller/QuestionController.java
@@ -0,0 +1,26 @@
+package com.ll.sbr.question.controller;
+
+import com.ll.sbr.question.model.Question;
+import com.ll.sbr.question.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/question")
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
