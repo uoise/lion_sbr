f21a422131858ea04d2c64d36043f7009f2bd0e2
uoise <yw823x@daum.net>
2023-03-27 14:36:16 +0900

    question_detail added

--- src/main/java/com/ll/sbr/question/controller/QuestionController.java
+++ src/main/java/com/ll/sbr/question/controller/QuestionController.java
@@ -1,11 +1,12 @@
 package com.ll.sbr.question.controller;
 
 import com.ll.sbr.question.model.Question;
-import com.ll.sbr.question.repository.QuestionRepository;
+import com.ll.sbr.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,12 +16,19 @@ import java.util.List;
 @RequestMapping("/question")
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = questionService.getQuestionList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable Long id) {
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
